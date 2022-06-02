bfc4a7e0e4e7dee7426b10b1a9e2a4af35f39b5b
Al-Bashir <Al-Bashir@users.noreply.github.com>
2022-06-02 04:59:43 +0200

    Fix nav menu responsive bug

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const menuIcon = document.querySelector('.nav-icon');
 const navElements = document.querySelectorAll('.nav-elements')[0];
 const navMenuList= document.querySelectorAll('.nav-menu-list');
+const mql = window.matchMedia('(max-width: 767px)');
+// mql.addEventListener("change", () => {
+//     console.log(document.querySelectorAll('.nav-elements')[0])
+//     navElements.style.display = 'block';
+// })
 menuIcon.addEventListener('click', () => {
     navElements.style.display = 'block';
     navElements.classList.add('nav-menu');
